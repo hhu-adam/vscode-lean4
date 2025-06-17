c15d1c94caddd35f98fc6da5afede30d2ad8a75b
Alexander Bentkamp <bentkamp@gmail.com>
2025-06-17 17:07:27 +0200

    fix error

--- vscode-lean4/src/leanclient.ts
+++ vscode-lean4/src/leanclient.ts
@@ -434,7 +434,7 @@ export class LeanClient implements Disposable {
         if (this.folderUri.scheme === 'file' && uri.scheme === 'file') {
             // lean4monaco: To avoid file system issues, we let any client manage any file:
             return true
-            return uri.isInFolder(this.folderUri)
+            // return uri.isInFolder(this.folderUri)
         }
         return false
     }
